6508f0d009936628a474156de9dc428fc411922d
valentinaloreto <vloretocastillo@gmail.com>
2019-09-24 16:13:48 +0200

    added sticky navbar

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,64 @@ window.onscroll = () => { makeMenuStickyOnScroll() };
 let menu = document.getElementById("menu");
 let sticky = menu.offsetTop;
 
-let makeMenuStickyOnScroll = () =>{ window.pageYOffset >= sticky ? menu.classList.add("sticky") : menu.classList.remove("sticky");  }
+
+
+let makeMenuStickyOnScroll = () =>{ 
+    console.log(sticky)
+    window.pageYOffset >= sticky ? menu.classList.add("sticky") : menu.classList.remove("sticky");  
+}
 
 
 // FORM VALIDATION
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let validateForm = () => {
 
-    var name = document.forms["registration"]["first_name"];               
+    var first_name = document.forms["registration"]["first_name"];     
+    var last_name = document.forms["registration"]["last_name"];            
    
-    if (name.value == "")                                  
-    { 
+    if (first_name.value == "") { 
         alert("Name is a required field"); 
         name.focus(); 
         return false; 
     } 
-    alert('good')
+
+    if (last_name.value == "") { 
+        alert("Name is a required field"); 
+        name.focus(); 
+        return false; 
+    } 
+
+
+    
 }
 
 
