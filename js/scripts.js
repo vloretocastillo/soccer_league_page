d27866361cd47e04c283c17d037bd1939f71ac87
valentinaloreto <vloretocastillo@gmail.com>
2019-09-25 10:11:21 +0200

    added funny button

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,53 @@
-// STICKY NAVBAR
+// STICKY NAVBAR && BACK TO TOP BUTTON 
 
-window.onscroll = () => { makeMenuStickyOnScroll() };
+window.onscroll = () => { 
+    makeMenuStickyOnScroll() 
+    backToTopOnScroll()
+};
         
 let menu = document.getElementById("menu");
 let sticky = menu.offsetTop;
+let backToTopBtn = document.getElementById("back-top-btn")
+let footer = document.getElementById("footer");
+let mainContainer = document.getElementById("policies");
 
 
+let makeMenuStickyOnScroll = () => { 
+    window.scrollY >= sticky ? menu.classList.add("sticky") : menu.classList.remove("sticky"); 
+}
 
-let makeMenuStickyOnScroll = () =>{ 
-    console.log(sticky)
-    window.pageYOffset >= sticky ? menu.classList.add("sticky") : menu.classList.remove("sticky");  
+let backToTopOnScroll = () => {
+
+    // console.log(sticky)
+    // console.log("window inner height:", window.innerHeight)
+    console.log("footer height: ", footer.offsetHeight)
+    // console.log("footer top: ", footer.offsetTop)
+    // console.log("window scroll y: ", window.scrollY)
+    // console.log("footer suma: ", footer.offsetTop + footer.offsetHeight)
+    // console.log("mainContainer: ", mainContainer.offsetTop )
+    // console.log(window.innerHeight, window.scrollY)
+    console.log("window.innerheight + window.scrollY: ", (footer.offsetTop + footer.offsetHeight) - (window.innerHeight + window.scrollY) )
+    console.log('-')
+
+    let someCalculation = (footer.offsetTop + footer.offsetHeight) - (window.innerHeight + window.scrollY)
+
+    
+
+    if (window.scrollY >= sticky) {
+        backToTopBtn.classList.remove("hide-me")
+    } else {
+        backToTopBtn.classList.add("hide-me")
+    }
+
+    if (footer.offsetHeight >= someCalculation) {
+        console.log('we have to hide it')
+        console.log(backToTopBtn.offsetTop)
+
+        backToTopBtn.classList.add("fix-me")
+    } else {
+        backToTopBtn
+    }
+
 }
 
 
